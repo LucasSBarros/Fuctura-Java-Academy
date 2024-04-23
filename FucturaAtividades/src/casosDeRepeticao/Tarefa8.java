3aa1e5392bebadf234fb4fe14267b5c7acf8c801
LucasSBarros <101149312+LucasSBarros@users.noreply.github.com>
2024-04-23 16:55:46 -0300

    Adição da resposta da tarefa 8 da lista de exercícios de repetição

--- FucturaAtividades/src/casosDeRepeticao/Tarefa8.java
+++ FucturaAtividades/src/casosDeRepeticao/Tarefa8.java
@@ -1,10 +1,47 @@
 package casosDeRepeticao;
 
+import java.util.Scanner;
+
 public class Tarefa8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int idade = 0, somatorio = 0, maiorIdade = 0, maior18 = 0, menor5 = 0;
+		double media = 0;
+		
 
+		for (int i = 0; i < 10; i++) {
+			Scanner sc = new Scanner(System.in);
+			
+			idade = sc.nextInt();
+			
+			somatorio += idade;
+			
+			if (idade > 18) {
+				
+				maior18++;
+				
+			} else if (idade < 5) {
+				
+				menor5++;
+				
+			}
+			
+			if (idade > maiorIdade) {
+				
+				maiorIdade = idade;
+				
+			}
+			
+		}
+		
+		media = somatorio/10;
+		
+		System.out.println("A média de idade do grupo é: " + media);
+		System.out.println("A quantidade de pessoas que tem mais de 18 é: " + maior18);
+		System.out.println("A quantidade de pessoas que tem menos de 5 é: " + menor5);
+		System.out.println("A maior idade lida foi: " + maiorIdade);
+		
 	}
 
 }
